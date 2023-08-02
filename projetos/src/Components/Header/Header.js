1e72aac9788d02a1486a1303a1552226502c6a3c
Viviane <vivivianesluzz@gmail.com>
2023-08-02 20:59:30 -0300

    feat:criação do header

---
+++ projetos/src/Components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+// import { Container } from './styles';
+
+function Header() {
+  return(
+    <header>
+      <nav>
+          <li>Sobre Mim</li>
+          <li>Habilidades</li>
+          <li>Formação</li>
+          <li>Projetos</li>
+      </nav>
+    </header>
+
+  ) 
+}
+
+export default Header;
+
