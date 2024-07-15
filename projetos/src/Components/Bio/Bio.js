422be44e4d3f32b552db47476345d21459a0ec9d
Viviane <vivivianesluzz@gmail.com>
2024-07-15 16:43:06 -0300

    Ajustes container formação

--- projetos/src/Components/Bio/Bio.js
+++ projetos/src/Components/Bio/Bio.js
@@ -8,7 +8,7 @@ function Bio() {
       <section className='infos'>
         <img className='emoji' src={Emoji} alt="" title=''/>
 
-        <p>Minha trajetória profissional iniciou na enfermagem, fiz a transição de carreira para a área de tecnologia, especializando-me como Front-end developer e já possuo experiência nessa área há um ano. Além disso, gosto muito de aprender, sair com amigos e explorar museus.</p>
+        <p className='trajetoria'>Minha trajetória profissional iniciou na enfermagem, fiz a transição de carreira para a área de tecnologia, especializando-me como Front-end developer e já possuo experiência nessa área há um ano. Além disso, gosto muito de aprender, sair com amigos e explorar museus.</p>
         {/* <p>Trabalhei durante seis anos na área da saúde, como técnica de Enfermagem e a pouco mais de um ano decidi migrar totalmente de carreira para o desenvolvimento web.</p> */}
   
       </section>
--- projetos/src/Components/Training/Training.js
+++ projetos/src/Components/Training/Training.js
@@ -1,21 +1,104 @@
 import React from 'react';
 import './Training.css';
+import MackenzieLogo from '../../assets/mackenzie-logo.png';
 
-export const Training = () => {
-    return (
-        <div className='class'>
-            <h1 className='training'>Formação</h1>
-            {/* <div className='card'>
-                <h2>Faculdade Presbiteriana Mackenzie</h2>
-                <h3>Graduação</h3>
-                <p>Graduação em Análise e desenvolvimento de sistemas</p>
-                <p>Estou cursando a graduação de Análise e desenvolvimento de sistemas na Universidade Presbiteriana Mackenzie,
-                com previsão de formatura em 2024.</p>
-            </div> */}
+
+
+const Courses = ({ logo, name, link, duration, title, description, className }) => (
+  <div className={`courses ${className}`}>
+    <div className='instituicao'>
+      <img className='logo' src={logo} alt={name} title={name} />
+      <div className='name'>
+        <a className='link' href={link}>{name}</a>
+        <span className='duration'>{duration}</span>
+      </div>
+    </div>
+    <p className='title'>{title}</p>
+    <p className='description'>{description}</p>
+  </div>
+);
+
+const Training = () => {
+  return (
+    <section className='training-section'>
+      <h4 className='training-title'>Formação</h4>
+
+      <div className='mack'>
+
+      <div className='courses-full'>
+        <div className='instituicao'>
+          <img className='logo' src={MackenzieLogo} alt="Mackenzie" title="Mackenzie" />
+          <div className='name'>
+            <a className='faculdade' href='https://www.mackenzie.br/'>Faculdade Presbiteriana Mackenzie</a>
+            <span className='duration'>Graduação</span>
+          </div>
         </div>
+        <p className='graduacao'>Graduação em Análise e desenvolvimento de Sistemas</p>
+        <p className='cursando'>
+          Estou cursando a graduação de Análise e Desenvolvimento de Sistemas na Universidade Presbiteriana Mackenzie,
+          com previsão de formatura em 2024.
+        </p>
+      </div>
+      </div>
 
-    );
-}
+      <div className='courses-container'>
+        <Courses
+          logo={MackenzieLogo}
+          name='Reprograma'
+          link='https://www.mackenzie.br/'
+          duration='Curso - 3 meses'
+          title='Bootcamp JavaScript'
+          description='Formação com duração de 6 meses com aulas ao vivo online. A dinâmica do curso seguia a mesma dinâmica de um time de tecnologia em uma startup moderna, com as principais cerimônias do Scrum.'
+        />
+
+        <Courses 
+          id="caelum"
+          logo={MackenzieLogo}
+          name='Caelum'
+          link='https://www.mackenzie.br/'
+          duration='Curso - 3 meses'
+          title='JavaScript para Front-end'
+          description='Formação com duração de 6 meses com aulas ao vivo online. A dinâmica do curso seguia a mesma dinâmica de um time de tecnologia em uma startup moderna, com as principais cerimônias do Scrum.'
+          className='caelum-background'
+
+        />
+        <Courses
+          logo={MackenzieLogo}
+          name='Reprograma'
+          link='https://www.mackenzie.br/'
+          duration='Curso - 3 meses'
+          title='Bootcamp JavaScript'
+          description='Formação com duração de 6 meses com aulas ao vivo online. A dinâmica do curso seguia a mesma dinâmica de um time de tecnologia em uma startup moderna, com as principais cerimônias do Scrum.'
+          className='ecc-background'
+        />
+        <div className='course-extra'>
+        <div className='courses-1'>
+            <div className='instituicao'>
+                <img className='logo' src={MackenzieLogo} alt="Mackenzie" title="Mackenzie" />
+                <div className=''>
+                <a className='faculdade' href='https://www.mackenzie.br/'>Faculdade Presbiteriana Mackenzie</a>
+                <span className='duration'>Curso livre</span>
+                </div>
+            </div>
+            <p className='graduacao'>Formação Front-end</p>
+            </div>
+
+        <div className='courses-1'>
+            <div className='instituicao'>
+                <img className='logo' src={MackenzieLogo} alt="Mackenzie" title="Mackenzie" />
+                <div className=''>
+                <a className='faculdade' href='https://www.mackenzie.br/'>Faculdade Presbiteriana Mackenzie</a>
+                <span className='duration'>Curso livre</span>
+                </div>
+            </div>
+            <p className='graduacao'>Formação Front-end</p>
+            </div>
+    </div>
+      </div>
+    </section>
+  );
+};
+
 
 
 export default Training;
