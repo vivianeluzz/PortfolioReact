4b837677e411a3c2b2505e1515bd354f75c9e89e
Viviane <vivivianesluzz@gmail.com>
2025-01-16 19:04:33 -0300

    Ajustes de dependencias

--- projetos/src/Components/Footer/Footer.js
+++ projetos/src/Components/Footer/Footer.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './Footer.css';
-import Git from '../../assets/Github_footer.png';
-import Linkedin from '../../assets/Linkedin_footer.png';
 
 const Footer = () => {
   return (
@@ -11,8 +9,6 @@ const Footer = () => {
       <div className='social-media'>
         <a className='text-contact' href='mailto:vivianesluzz@gmail.com'>vivianesluzz@gmail.com</a>
         <a className='text-contact' href='tel:+5511986966179'>11 948746424</a>
-        {/* <a href="https://github.com/vivianeluzz"> <img className='social' src={Git} alt="Foto link do Github" title='Github'/></a> */}
-        {/* <a href="https://www.linkedin.com/in/viviane-luz/"> <img className='social' src={Linkedin} alt="Foto link do Linkedin" title='Linkedin'/></a> */}
       </div>
     </div>
     </footer>
