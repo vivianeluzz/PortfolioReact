9e79f5b44f4d421f8ca546ba8ab5826f3247cb65
Viviane <vivivianesluzz@gmail.com>
2025-01-16 21:00:43 -0300

    Resolvendo bugs de pdf curriculo deletando

--- projetos/src/Components/Home/Home.js
+++ projetos/src/Components/Home/Home.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './Home.css';
 import Avatar from '../../assets/avatar.jpg';
-import Download from '../../assets/download.svg';
 import Git from '../../assets/git.png';
 import Linkedin from '../../assets/linkedin.png';
 import Vetor from '../../assets/vetor.png';
-import Curr from '../../assets/curremoji.svg';
-import Emoji from '../../assets/emojiavatar.png';
-import Curriculo from '../../assets/VivianeLuz.pdf';
+
 
 export const Home = () => {
 
@@ -23,16 +20,9 @@ export const Home = () => {
                 </div>
                 <main className='item-vetor'>
                     <img className='vetor' src={Vetor} alt="Vetor" title='' />
-                    <a className="git" href={Curriculo} download="Currículo - Viviane Luz.pdf"> <img className='download' src={Download} alt="" title='' />
-                        Baixe meu currículo</a>
+                    
                     <a href="https://github.com/vivianeluzz"> <img className='social' src={Git} alt="" title='' /></a>
                     <a href="https://www.linkedin.com/in/viviane-luz/"> <img className='social' src={Linkedin} alt="" title='' /></a>
-                    <a href={Curriculo} download="Currículo - Viviane Luz.pdf"> <img src={Curriculo} alt="" title='' />
-                        <div className='botao-rotativo'>
-                            <img className='circulo-rotativo' src={Curr} alt="Botao rotativo para baixar curriculo" title='' />
-                            <img className='emoji-curriculo' src={Emoji} alt="" title='' />
-                        </div>
-                    </a>
                 </main>
             </div>
         </section>
